78da1ddb5c8fb730340544f8469f84a9ec3a1374
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2020-11-07 16:35:05 -0500

    Updates

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-alert("hello world");
-// var location = document.getElementById("location-data");
+window.onload = function() {
+    var location = document.getElementById("location-data");
+    // var data = data.json;
+    // var obj = JSON.parse(data);
+    // location.innerHTML = obj.location;
+    console.log("hello");
+}
 
 // var obj = JSON.parse("data.json");
 // location.innerHTML = obj.location;
-console.log("hello");
-
-// var xmlhttp = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function() {
-//   if (this.readyState == 4 && this.status == 200) {
-//     var myObj = JSON.parse(this.responseText);
-//     document.getElementById("demo").innerHTML = myObj.name;
-//   }
-// };
-// xmlhttp.open("GET", "data.json", true);
-// xmlhttp.send();
