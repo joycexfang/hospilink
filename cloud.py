ebbdd2e8d22c04bd5d2cc8718bdbd10804304e48
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2020-11-07 15:22:02 -0500

    Updates

--- cloud.py
+++ cloud.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from cloudant.client import Cloudant
 from cloudant.error import CloudantException
 from cloudant.result import Result, ResultByKey
@@ -29,9 +31,30 @@ print("Location: {0}".format(result_collection[0][0].get("doc").get("location"))
 
 # write-html.py
 
-f = open('index.html', 'a')
+# f = open('index.html', 'a')
 
-message = '<p>hello world</p>'
+# message = '<p>hello world</p>'
 
-f.write(message)
-f.close()
+# f.write(message)
+# f.close()
+
+
+html_str = """
+<table>
+     <tr>
+       <th>Number</th>
+       <th>Square</th>
+     </tr>
+     <indent>
+     <% for i in range(10): %>
+       <tr>
+         <td><%= i %></td>
+         <td><%= i**2 %></td>
+       </tr>
+     </indent>
+</table>
+"""
+
+html_file= open("index.html","w")
+html_file.write(html_str)
+html_file.close()
