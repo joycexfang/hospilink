78422deff8395491c7f010e674b202582534aba4
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2020-11-07 14:42:48 -0500

    Getting started

---
+++ cloud.py
@@ -0,0 +1,21 @@
+from cloudant.client import Cloudant
+from cloudant.error import CloudantException
+from cloudant.result import Result, ResultByKey
+
+# set up
+# serviceUsername = "f5bdda05-9289-4541-9665-0290ff2dd594-bluemix"
+# servicePassword = ""
+# serviceURL = "https://f5bdda05-9289-4541-9665-0290ff2dd594-bluemix.cloudantnosqldb.appdomain.cloud"
+
+ACCOUNT_NAME = "f5bdda05-9289-4541-9665-0290ff2dd594-bluemix"
+API_KEY = "z_YFZaGQ3JmiXrLccCmqV8SeWxJz-SlMCiazZjB6aM6r"
+
+client = Cloudant.iam(ACCOUNT_NAME, API_KEY, connect=True)
+# client = Cloudant(serviceUsername, servicePassword, url=serviceURL)
+client.connect()
+
+databaseName = "our-database"
+myDatabaseDemo = client.create_database(databaseName)
+if myDatabaseDemo.exists():
+    print("'{0}' successfully created.\n".format(databaseName))
+
