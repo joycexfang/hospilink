4ccfc989e3120d91eb71ebee8d28d1f7c8595107
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2020-11-07 14:51:20 -0500

    Print document

--- cloud.py
+++ cloud.py
@@ -2,11 +2,7 @@ from cloudant.client import Cloudant
 from cloudant.error import CloudantException
 from cloudant.result import Result, ResultByKey
 
-# set up
-# serviceUsername = "f5bdda05-9289-4541-9665-0290ff2dd594-bluemix"
-# servicePassword = ""
-# serviceURL = "https://f5bdda05-9289-4541-9665-0290ff2dd594-bluemix.cloudantnosqldb.appdomain.cloud"
-
+# set up 
 ACCOUNT_NAME = "f5bdda05-9289-4541-9665-0290ff2dd594-bluemix"
 API_KEY = "z_YFZaGQ3JmiXrLccCmqV8SeWxJz-SlMCiazZjB6aM6r"
 
@@ -19,3 +15,7 @@ myDatabaseDemo = client.create_database(databaseName)
 if myDatabaseDemo.exists():
     print("'{0}' successfully created.\n".format(databaseName))
 
+# Simple and full retrieval of the first
+# document in the database.
+result_collection = Result(myDatabaseDemo.all_docs, include_docs=True)
+print("Retrieved full document:\n{0}\n".format(result_collection[0]))
